50721abb3b7e139c5568bda50b914fc42f842d98
Dave Worthington <daveworthington@lawn-143-215-53-205.lawn.gatech.edu>
2019-01-18 22:11:51 -0500

    Added login and not yet working logout

--- talli-app/src/components/MainPage.js
+++ talli-app/src/components/MainPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Button, ListItem, Grid } from '@material-ui/core';
+import GoogleLogin from 'react-google-login';
 import { navigate } from 'react-mini-router';
 import './component_style/MainPage.css';
 
@@ -10,6 +11,13 @@ import './component_style/MainPage.css';
  */
 export default class MainPage extends React.Component {
     ChangeView(page) { navigate(page); }
+
+    constructor() {
+        super();
+        this.state = {
+            loggedIn: false,
+        };
+    }
     
     render() {
         return(
@@ -22,11 +30,27 @@ export default class MainPage extends React.Component {
                         </ListItem>
                         
                         <ListItem>
-                            <Button variant="contained" color="primary" className="buttons" onClick={() => this.ChangeView('/organizer')}>Organizer Login</Button>
+                            {/* <Button variant="contained" color="primary" className="buttons" onClick={() => this.ChangeView('/organizer')}>Organizer Login</Button> */}
+                            <GoogleLogin 
+                                clientId="1061225539650-cp3lrdn3p1u49tsq320l648hcuvg8plb.apps.googleusercontent.com"
+                                render={renderProps => (
+                                    <Button variant="contained" color="secondary" className="buttons" onClick={renderProps.onClick}>Organizer Login (Google)</Button>
+                                )}
+                                onSuccess={this.success.bind(this)}
+                                onFailure={this.failure.bind(this)} />
                         </ListItem>
                     </div>
                 </Grid>
             </div>
         );
+    }
+
+    success() {
+        this.setState({loggedIn: true});
+        this.ChangeView('/organizer');
+    }
+
+    failure() {
+        console.log("Login failed");
     }
 }
--- talli-app/src/components/Organizer.js
+++ talli-app/src/components/Organizer.js
@@ -3,6 +3,8 @@ import NewEvent from './OrganizerView/NewEventForm';
 import EventList from './OrganizerView/EventList';
 import AddEntry from './OrganizerView/AddEntryOrg';
 import ViewEvent from './OrganizerView/ViewEvent';
+import { GoogleLogout } from 'react-google-login';
+import { navigate } from 'react-mini-router';
 import './component_style/Organizer.css';
 
 const orgViews = {
@@ -19,23 +21,39 @@ export default class Organizer extends React.Component {
     constructor(props) {
         super(props);
         this.state = { curView: orgViews.MAIN };
-        this.changeView = this.changeView.bind(this);
+        this.setView = this.setView.bind(this);
     }
 
-    changeView(newView) {
+    ChangeView(page) {
+        navigate(page);
+    }
+
+    setView(newView) {
         this.setState({ curView: newView });
+    }
+
+    logout(response) {
+        console.log(response);
+        this.ChangeView('/');
     }
 
     render() {
         switch(this.state.curView) {
             case orgViews.CREATE:
-                return( <NewEvent orgViews={orgViews} handler={this.changeView}/> );
+                return( <NewEvent orgViews={orgViews} handler={this.setView}/> );
             case orgViews.ADD:
-                return( <AddEntry orgViews={orgViews} handler={this.changeView}/> );
+                return( <AddEntry orgViews={orgViews} handler={this.setView}/> );
             case orgViews.VIEW:
-                return ( <ViewEvent orgViews={orgViews} handler={this.changeView}/> );
+                return( <ViewEvent orgViews={orgViews} handler={this.setView}/> );
             default:
-                return( <EventList orgViews={orgViews} handler={this.changeView}/> );
+                return( 
+                    <div className="main">
+                        <GoogleLogout 
+                            buttonText="Logout"
+                            onLogoutSuccess={this.logout.bind(this)} />
+                        <EventList orgViews={orgViews} handler={this.setView}/> 
+                    </div>
+                );
         }
     }
 }
