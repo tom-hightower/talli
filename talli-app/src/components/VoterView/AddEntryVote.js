2ba2493844ff3b8d2729db7bbca12a5522f8630e
Tom Hightower <tom.hightower356@gmail.com>
2019-01-23 15:58:56 -0500

    Added confirmation dialog and submit button to JoinEvent and AddEntryVote

--- talli-app/src/components/VoterView/AddEntryVote.js
+++ talli-app/src/components/VoterView/AddEntryVote.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Typography, TextField } from '@material-ui/core';
+import { Typography, TextField, Button } from '@material-ui/core';
 import QrReader from 'react-qr-reader';
+import EntryConfirmation from './EntryConfirmation';
 import '../component_style/Voter.css';
 
 /**
@@ -9,42 +10,77 @@ import '../component_style/Voter.css';
 export default class AddEntryVote extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { entryID: '' };
+        /** ::STATE INFO::
+         *  entryID:        Entry's UID, obtained either from QRcode or textfield
+         *  idFieldValue:   The value currently in the textbox
+         */
+        this.state = { entryID: '', idFieldValue: '' };
         this.handleScan = this.handleScan.bind(this);
         this.handleError = this.handleError.bind(this);
         this.handleText = this.handleText.bind(this);
         this.handleAddEntry = this.handleAddEntry.bind(this);
+        this.keyPress = this.keyPress.bind(this);
+
+        this.confirmChild = React.createRef();
+    }
+
+    //TODO: server request to return Entry name for given state.entryID
+    getNameFromID() {
+        return(this.state.entryID);
+    }
+
+    requestConfirm = () => {
+        this.confirmChild.current.handleOpen();
     }
 
     handleScan(data) {
         if (data && data.length > 4) {
             this.setState({ entryID: data });
-            this.handleAddEntry();
+            this.requestConfirm();
         }
     }
 
-    handleText(event) {
-        event.persist();
-        if (event.target && event.target.value.length > 5) {
-            this.setState({ entryID: event.target.value });
-            this.handleAddEntry();
+    handleText() {
+        if (this.state.idFieldValue.length > 5) {
+            this.setState({ entryID: this.state.idFieldValue });
+            this.requestConfirm();
         }
     }
 
     handleAddEntry() {
-        // Add the Entry and return to ranking
+        //TODO: Add the Entry and return to ranking
         this.props.handler(this.props.voteViews.RANK);
     }
 
     handleError(err) {}
 
+    keyPress(e) {
+        if(e.key === 'Enter') {
+            this.handleText();
+        }
+    }
+
     render() {
         return(
             <div>
+                <EntryConfirmation entryName={this.getNameFromID()} ref={this.confirmChild} handler={this.handleAddEntry}/>
                 <QrReader delay={300} onScan={this.handleScan} onError={this.handleError} style={{ width: '80%', margin: '20px auto 0px'}} />
                 <Typography variant='h5' align='center' className="QRText">Scan QR Code or enter Entry ID:</Typography>
                 <div className="textField">
-                    <TextField id="outlined-dense" label="Entry ID" margin="dense" variant="outlined" onChange={this.handleText}/>
+                    <TextField 
+                        id="outlined-dense" 
+                        label="Entry ID" 
+                        margin="dense" 
+                        variant="outlined" 
+                        value={this.state.idFieldValue} 
+                        onKeyPress={this.keyPress}
+                        onChange={e => this.setState({ idFieldValue: e.target.value })}
+                    />
+                </div>
+                <div className="submitButtonContainer">
+                    <Button variant="contained" color="default" className="homeButton" onClick={this.handleText}>
+                        Add
+                    </Button>
                 </div>
             </div>
         );
