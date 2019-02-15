2cc97ac21ec46d29e9bd2e583abd95d83f59ef86
Tom Hightower <tom.hightower356@gmail.com>
2019-02-15 10:28:54 -0500

    restored nick_login versions of NavBar and ViewEvent

--- talli-app/client/src/components/OrganizerView/ViewEvent.js
+++ talli-app/client/src/components/OrganizerView/ViewEvent.js
@@ -90,7 +90,7 @@ export default class ViewEvent extends React.Component {
     }
 
     goBack = () => {
-        if (this.state.view === 'main') {
+        if (this.state.view === 'main' || this.state.view === "results") {
             this.props.handler(this.props.orgViews.MAIN);
         } else if (this.state.view === 'entries') {
             this.setState({
@@ -100,47 +100,100 @@ export default class ViewEvent extends React.Component {
         }
     }
 
+    viewResults = () => {
+        this.setState({
+            view: "results"
+        })
+    }
+
+    manageEvent = () => {
+        this.setState({
+            view: "main"
+        })
+    }
+
+
+
     render() {
         return (
             <div className="main">
-                <ExportOrgData ref={this.exportChild} event={this.state.event}/>
-                <EditEntries ref={this.entryChild} event={this.state.event} googleId={this.props.user.googleId}/>
-                <AddEntries ref={this.addChild} event={this.state.event} googleId={this.props.user.googleId}/>
-                <EditEvent ref={this.eventChild} event={this.state.event} googleId={this.props.user.googleId}/>
-                <EditVoting ref={this.votingChild} event={this.state.event} googleId={this.props.user.googleId}/>
-                <Typography variant="h3" align='center' gutterBottom>{this.state.event.name}</Typography>
-                <div className="options">
-                    <Button className="button1" variant="contained" color="primary">Manage Event</Button>
-                    <Button className="button1" variant="contained">View Results</Button>
-                </div>
+                {
+                    this.props.user != null &&
+                    <div> 
+                        <ExportOrgData ref={this.exportChild} event={this.state.event}/>
+                        <EditEntries ref={this.entryChild} event={this.state.event} googleId={this.props.user.googleId}/>
+                        <AddEntries ref={this.addChild} event={this.state.event} googleId={this.props.user.googleId}/>
+                        <EditEvent ref={this.eventChild} event={this.state.event} googleId={this.props.user.googleId}/>
+                        <EditVoting ref={this.votingChild} event={this.state.event} googleId={this.props.user.googleId}/>
+                        <Typography variant="h3" align='center' gutterBottom>{this.state.event.name}</Typography>
+                    </div>
+                }
                 { 
                     this.state.view === 'main' && 
-                    <div className="box">
-                        <Button className="listButtons" onClick={this.handleExport}>Export Event & Entry QR Codes</Button>
-                        <Button className="listButtons" onClick={this.handleOpenEntries}>View/Add/Edit Entries</Button>
-                        <Button className="listButtons" onClick={this.handleEventEdit}>View/Edit Event Details</Button>
-                        <Button className="listButtons" onClick={this.handleOpenCloseVoting}>Open/Close Voting</Button>
-                    </div> 
+                    <div>
+                        <div className="options">
+                            <Button className="button1" variant="contained" color="primary" onClick={this.manageEvent}>Manage Event</Button>
+                            <Button className="button1" variant="contained" onClick={this.viewResults}>View Results</Button>
+                        </div>
+                        <div className="box">
+                            <Button className="listButtons" onClick={this.handleExport}>Export Event & Entry QR Codes</Button>
+                            <Button className="listButtons" onClick={this.handleOpenEntries}>View/Add/Edit Entries</Button>
+                            <Button className="listButtons" onClick={this.handleEventEdit}>View/Edit Event Details</Button>
+                            <Button className="listButtons" onClick={this.handleOpenCloseVoting}>Open/Close Voting</Button>
+                        </div> 
+                    </div>
                 }
                 { 
                     this.state.view === 'entries' && this.state.event.entries !== undefined &&
-                    <div className="box">
-                        {
-                            
-                            Object.values(this.state.event.entries).map((entry, index) => 
-                                <Button className="listButtons" onClick={() => this.handleEntryEdit(entry.id)}>
-                                    {entry.title} by {entry.presenters}
-                                </Button>
-                            )
-                        }
-                         <Button className="listButtons" color="primary" onClick={this.handleAddEntry}>Add New Entry</Button>
-                    </div> 
+                    <div>
+                        <div className="options">
+                            <Button className="button1" variant="contained" color="primary" onClick={this.manageEvent}>Manage Event</Button>
+                            <Button className="button1" variant="contained" onClick={this.viewResults}>View Results</Button>
+                        </div>
+                        <div className="box">
+                            {
+                                
+                                Object.values(this.state.event.entries).map((entry, index) => 
+                                    <Button className="listButtons" onClick={() => this.handleEntryEdit(entry.id)}>
+                                        {entry.title} by {entry.presenters}
+                                    </Button>
+                                )
+                            }
+                            <Button className="listButtons" color="primary" onClick={this.handleAddEntry}>Add New Entry</Button>
+                        </div> 
+                    </div>
                 }
                 {
                    this.state.view === 'entries' && this.state.event.entries === undefined &&
-                    <div className="box">
-                         <Button className="listButtons" color="primary" onClick={this.handleAddEntry}>Add New Entry</Button>
-                    </div>  
+                   <div>
+                        <div className="options">
+                            <Button className="button1" variant="contained" color="primary" onClick={this.manageEvent}>Manage Event</Button>
+                            <Button className="button1" variant="contained" onClick={this.viewResults}>View Results</Button>
+                        </div>
+                        <div className="box">
+                            <Button className="listButtons" color="primary" onClick={this.handleAddEntry}>Add New Entry</Button>
+                        </div>  
+                   </div>
+                }
+                {
+                    this.state.view === "results" &&
+                    <div className="viewResults">
+                        <div className="options">
+                            <Button className="button1" variant="contained" onClick={this.manageEvent}>Manage Event</Button>
+                            <Button className="button1" variant="contained" color="primary" onClick={this.viewResults}>View Results</Button>
+                        </div>
+                        <Typography variant="h5">Set up google sheets for results:</Typography>
+                        <br />
+                        <div className="instructions">
+                            <div>1. Create a Google Sheet in your desired location</div>
+                            <div>
+                                {/* TODO: This should automatically save, probably to firebase */}
+                                2. Grab the spreadsheet ID from the URL and paste it here: <input className="sheetId" placeholder="Google sheet ID"></input>
+                                <div className="note">https://docs.google.com/spreadsheets/d/<b>SPREADSHEET ID</b>/edit#gid=0</div>
+                            </div>
+                            <div>3. Share the spreadsheet with editing rights with <b>talli-455@talli-229017.iam.gserviceaccount.com</b></div>
+                        </div>
+                    </div>
                 }
                 <br />
                 <Button
