af915dcdfa063681fa553e8fc77cb6ac021d1039
Tom Hightower <tom.hightower356@gmail.com>
2019-02-15 13:19:04 -0500

    fixed dependency issues

--- talli-app/server.js
+++ talli-app/server.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 // const authRoutes = require('./routes/auth-routes');
-var GoogleSpreadsheet = require('google-spreadsheet');
-var creds = require('./client_secret.json');
+let GoogleSpreadsheet = require('google-spreadsheet');
+let creds = require('./client_secret.json');
 
-var urlGoogle = require('./google-util');
+let urlGoogle = require('./google-util');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,17 +14,18 @@ app.use(bodyParser.json());
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-var doc = new GoogleSpreadsheet('1k1r8EvCTuRBcamM71lYSw3OK7cBwehHXFLGe2kFRy50');
+let doc = new GoogleSpreadsheet('1k1r8EvCTuRBcamM71lYSw3OK7cBwehHXFLGe2kFRy50');
 
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     // console.log('a user connected');
     // socket.on('disconnect', () => {
     //     console.log('User Disconnected');
     // });
-    
+
     socket.on('add_data', (data) => {
-        console.log('message: ' + data);
+        let logString = 'message: ' + data;
+        console.log(logString);
         doc.useServiceAccountAuth(creds, (err) => {
             console.log(err);
             doc.addRow(1, data, (err) => {
@@ -42,4 +43,4 @@ io.listen(port);
 
 // app.use('/auth', authRoutes);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
