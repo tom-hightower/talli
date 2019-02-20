6406fc0b8c3352d935a578b6bb95e26e92c1da66
Tom Hightower <tom.hightower356@gmail.com>
2019-02-20 16:37:37 -0500

    Style changes that don't break it

--- talli-app/client/src/components/VoterView/RankingContainer.js
+++ talli-app/client/src/components/VoterView/RankingContainer.js
@@ -11,17 +11,20 @@ import '../component_style/SubmitContainer.css';
 const DragHandle = SortableHandle(() => <span> <SliderIcon className="Sliders" /></span>);
 
 const SortableItem = SortableElement(({ value, item }) =>
-    <li className="rankings">{item + 1} |  {value}<DragHandle /></li>
+    <li className="rankings">
+        {item + 1} |  {value}
+        <DragHandle />
+    </li>
 );
 
 const SortableList = SortableContainer(({ items }) => {
     return (
-        <ol>
+        <ul>
             {items.length !== 0 ? <div></div> : <div>Tap the Plus to add an entry</div>}
             {items.map((value, index) => (
                 <SortableItem key={`item-${index}`} item={index} index={index} value={value.name} />
             ))}
-        </ol>
+        </ul>
     );
 });
 
@@ -81,7 +84,7 @@ export default class SortContainer extends Component {
                         entries: eventEntries
                     },
                     items: itemList
-                }, () => { this.props.updateItemsHandler(this.state.items)});
+                }, () => { this.props.updateItemsHandler(this.state.items) });
             });
         });
     }
