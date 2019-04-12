1cedfdb4ac577fb8d34cfa5b2c4f5e5e42fb5d77
Dave Worthington <daveworthington@lawn-143-215-56-131.lawn.gatech.edu>
2019-04-12 00:14:47 -0400

    added sheet creation to event creation process

--- talli-app/client/src/components/OrganizerView/AddEntryOrg.js
+++ talli-app/client/src/components/OrganizerView/AddEntryOrg.js
@@ -68,7 +68,7 @@ export default class AddEntryOrg extends Component {
                 itemsRef.child(item.id).set(item);
             }
         }
-        this.props.handler(this.props.orgViews.MAIN);
+        this.props.handler(this.props.orgViews.SHEET);
     }
 
     readCSV = event => {
@@ -160,7 +160,7 @@ export default class AddEntryOrg extends Component {
                     >
                         Cancel
                     </Button>
-                    <Button type="submit" variant="contained" color="primary" className="buttons">Done</Button>
+                    <Button type="submit" variant="contained" color="primary" className="buttons">Next</Button>
                 </form>
             </div>
         );
